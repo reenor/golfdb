552bc1ea1e12dc699f8f1cc9bdf72a7e5e16a9c6
DESKTOP-DNTA4BT\lokati <reedus.chung@gmail.com>
2024-08-30 12:09:34 +0900

    no message

--- test_video.py
+++ test_video.py
@@ -8,6 +8,10 @@ from model import EventDetector
 import numpy as np
 import torch.nn.functional as F
 
+import os
+import os.path as osp
+import subprocess
+
 event_names = {
     0: 'Address',
     1: 'Toe-up',
@@ -19,6 +23,72 @@ event_names = {
     7: 'Finish'
 }
 
+def main(args):
+    seq_length = args.seq_length
+
+    print('Preparing video: {}'.format(args.path))
+
+    ds = SampleVideo(args.path, transform=transforms.Compose([ToTensor(),
+                                Normalize([0.485, 0.456, 0.406],
+                                          [0.229, 0.224, 0.225])]))
+
+    dl = DataLoader(ds, batch_size=1, shuffle=False, drop_last=False)
+
+    model = EventDetector(pretrain=True,
+                          width_mult=1.,
+                          lstm_layers=1,
+                          lstm_hidden=256,
+                          bidirectional=True,
+                          dropout=False)
+
+    try:
+        save_dict = torch.load('models/swingnet_2000.pth.tar')
+    except:
+        print("Model weights not found. Download model weights and place in 'models' folder. See README for instructions")
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device:', device)
+    model.load_state_dict(save_dict['model_state_dict'])
+    model.to(device)
+    model.eval()
+    print("Loaded model weights")
+
+    print('Testing...')
+    for sample in dl:
+        images = sample['images']
+        # full samples do not fit into GPU memory so evaluate sample in 'seq_length' batches
+        batch = 0
+        while batch * seq_length < images.shape[1]:
+            if (batch + 1) * seq_length > images.shape[1]:
+                image_batch = images[:, batch * seq_length:, :, :, :]
+            else:
+                image_batch = images[:, batch * seq_length:(batch + 1) * seq_length, :, :, :]
+            logits = model(image_batch.cuda())
+            if batch == 0:
+                probs = F.softmax(logits.data, dim=1).cpu().numpy()
+            else:
+                probs = np.append(probs, F.softmax(logits.data, dim=1).cpu().numpy(), 0)
+            batch += 1
+
+    events = np.argmax(probs, axis=0)[:-1]
+    print('Predicted event frames: {}'.format(events))
+    
+
+    confidence = []
+    for i, e in enumerate(events):
+        confidence.append(probs[e, i])
+    print('Condifence: {}'.format([np.round(c, 3) for c in confidence]))
+    
+    cap = cv2.VideoCapture(args.path)
+    for i, e in enumerate(events):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, e)
+        _, img = cap.read()
+        cv2.putText(img, '{:.3f}'.format(confidence[i]), (20, 20), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 255))
+        cv2.imshow(event_names[i], img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    cap.release()
 
 class SampleVideo(Dataset):
     def __init__(self, path, input_size=160, transform=None):
@@ -62,67 +132,5 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--path', help='Path to video that you want to test', default='test_video.mp4')
     parser.add_argument('-s', '--seq-length', type=int, help='Number of frames to use per forward pass', default=64)
     args = parser.parse_args()
-    seq_length = args.seq_length
 
-    print('Preparing video: {}'.format(args.path))
-
-    ds = SampleVideo(args.path, transform=transforms.Compose([ToTensor(),
-                                Normalize([0.485, 0.456, 0.406],
-                                          [0.229, 0.224, 0.225])]))
-
-    dl = DataLoader(ds, batch_size=1, shuffle=False, drop_last=False)
-
-    model = EventDetector(pretrain=True,
-                          width_mult=1.,
-                          lstm_layers=1,
-                          lstm_hidden=256,
-                          bidirectional=True,
-                          dropout=False)
-
-    try:
-        save_dict = torch.load('models/swingnet_1800.pth.tar')
-    except:
-        print("Model weights not found. Download model weights and place in 'models' folder. See README for instructions")
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print('Using device:', device)
-    model.load_state_dict(save_dict['model_state_dict'])
-    model.to(device)
-    model.eval()
-    print("Loaded model weights")
-
-    print('Testing...')
-    for sample in dl:
-        images = sample['images']
-        # full samples do not fit into GPU memory so evaluate sample in 'seq_length' batches
-        batch = 0
-        while batch * seq_length < images.shape[1]:
-            if (batch + 1) * seq_length > images.shape[1]:
-                image_batch = images[:, batch * seq_length:, :, :, :]
-            else:
-                image_batch = images[:, batch * seq_length:(batch + 1) * seq_length, :, :, :]
-            logits = model(image_batch.cuda())
-            if batch == 0:
-                probs = F.softmax(logits.data, dim=1).cpu().numpy()
-            else:
-                probs = np.append(probs, F.softmax(logits.data, dim=1).cpu().numpy(), 0)
-            batch += 1
-
-    events = np.argmax(probs, axis=0)[:-1]
-    print('Predicted event frames: {}'.format(events))
-    cap = cv2.VideoCapture(args.path)
-
-    confidence = []
-    for i, e in enumerate(events):
-        confidence.append(probs[e, i])
-    print('Condifence: {}'.format([np.round(c, 3) for c in confidence]))
-
-    for i, e in enumerate(events):
-        cap.set(cv2.CAP_PROP_POS_FRAMES, e)
-        _, img = cap.read()
-        cv2.putText(img, '{:.3f}'.format(confidence[i]), (20, 20), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 255))
-        cv2.imshow(event_names[i], img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-
+    main(args)
